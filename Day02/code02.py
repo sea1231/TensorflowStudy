7bf23a4c19edc8956377286630fc56d35d0dc2a3
TaegyeongLee <luckperson7@naver.com>
2018-01-06 14:46:45 +0900

    code02, shuffle list and unzip list

--- Day02/code02.py
+++ Day02/code02.py
@@ -12,6 +12,7 @@ IMAGE_DIR_BASE = "../animal_images"
 image_dir_list = os.listdir(IMAGE_DIR_BASE)
 print(image_dir_list)
 
+
 # image load function
 def load_image(addr):
     img = cv2.imread(addr) # load image
@@ -20,19 +21,28 @@ def load_image(addr):
     img = img.astype(np.float32) # type change
     return img
 
-
 # image load
 class_index = 0
-feature = []
-label = []
+features = []
+labels = []
 
 for dir_name in image_dir_list:
     image_file_list = os.listdir(IMAGE_DIR_BASE + os.sep + dir_name) # image folder
     for file_name in image_file_list:
         image = load_image(IMAGE_DIR_BASE + os.sep + dir_name + os.sep + file_name)
-        feature.append(image.ravel()) # image 를 1차원 벡터로 변환
-        label.append(class_index)
+        features.append(image.ravel()) # image 를 1차원 벡터로 변환
+        labels.append(class_index)
     class_index += class_index
+
+# shuffle after zip then unzip
+zip_list = list(zip(features, labels))
+shuffle(zip_list)
+features, labels = zip(*zip_list)
+
+print(type(features))
+
+features = np.array(features) # tuple -> np.array
+labels = np.array(labels) # tuple -> np.array
 
 
 
